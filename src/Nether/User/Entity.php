8e304e4add7ccf7c65e4b1c8ab490d692375eebf
bobmagicii <bob@majdak.net>
2022-11-08 23:37:38 +0000

    thufar:

---
+++ src/Nether/User/Entity.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Nether\User;
+use Nether;
+
+#[Nether\Database\Meta\TableClass('Users')]
+class Entity
+extends Nether\Object\Prototype {
+
+
+
+}
---
+++ src/Nether/User/Library.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Nether\User;
+use Nether;
+
+class Library {
+
+	static public function
+	Init(Nether\Object\Datastore $Config, ...$Argv):
+	void {
+
+		return;
+	}
+
+}
