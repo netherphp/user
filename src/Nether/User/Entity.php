ae41dfb68abec103ab726e040d349c23db2a4348
bobmagicii <bob@majdak.net>
2022-11-09 01:28:28 +0000

    thusfar

--- src/Nether/User/Entity.php
+++ src/Nether/User/Entity.php
@@ -7,6 +7,17 @@ use Nether;
 class Entity
 extends Nether\Object\Prototype {
 
+	#[Nether\Database\Meta\TypeIntBig(Unsigned: TRUE, AutoInc: TRUE)]
+	#[Nether\Database\Meta\PrimaryKey]
+	public int
+	$ID;
 
+	#[Nether\Database\Meta\TypeVarChar(Size: 24)]
+	public string
+	$Alias;
+
+	#[Nether\Database\Meta\TypeVarChar(Size: 255)]
+	public string
+	$Email;
 
 }
